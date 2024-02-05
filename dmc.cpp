e32f027d970abbe75b0a36a3fb05be74a0e4bd02
Cañón <jjcanond@eafit.edu.co>
2024-02-04 19:05:27 -0500

    lectura manejos de la terminal

--- dmc.cpp
+++ dmc.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+#include <thread>
+#include <sndfile.h>
+
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
     if (argc > 0)
     {
-        std::string parametro1 = argv[1];
-        std::string parametro2 = argv[2];
+        string parametro1 = argv[1];
+        string parametro2 = argv[2];
 
-        std::cout << parametro1 << parametro2;
+        cout << parametro1 << " " << parametro2;
     }
 
     return 0;
