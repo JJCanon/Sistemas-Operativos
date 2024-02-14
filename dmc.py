8289a301e1b1a06d46f5324525c1a737731790af
Cañón <jjcanond@eafit.edu.co>
2024-02-14 11:05:47 -0500

    creacion de funciones para trasformar archivos en ruta especificada, dos temporales y una definitiva

--- dmc.py
+++ dmc.py
@@ -1,8 +1,15 @@
+#Librerías
 from pydub import AudioSegment as AS #Manejos de Audio
 import argparse as arg #Manejo de parametro desde terminal
 import os # Manejo de Archivos
+import aifc # Manejo de Archivos de tipo AIF 
 
-#funcion para ver los archivos de una carpeta
+
+# Variables Globales
+
+#Funciones
+
+#Funcion para ver los archivos de una carpeta
 def archivosCarpeta(nameFolder):
     
     # Listar todos los archivos en el directorio (la carpeta debe estar en la misma ruta del programa)
@@ -10,8 +17,32 @@ def archivosCarpeta(nameFolder):
 
     return archivos
 
-if __name__=="__main__":
+#Funcion para transformar archivo aiff a mp3(funcion temporal)
+def aiff_mp3(archivoAif,extension):
+    # Cargar archivo aiff
+    audioAiff=AS.from_file(archivoAif,format="aiff")
+    # Transformar archivo a mp3
+    audioAiff.export("cancionMp3.mp3",format=extension)
+
+#funcion para transformar archivos aiff a wav(funcion temporal)
+def aiff_wav(archivoAif,extension):
+    #cargar archivo aiff
+    audioAiff=AS.from_file(archivoAif,format="aiff")
+    #transformar el archivo a wav
+    audioAiff.export("cancionWav.wav",format=extension)
+
+# Funcion que transforma los archivos en el formato especificado
+def transformarAudio(nameFile,extension=None):
+    # separar nombre y formato del archivo de entrada
+    name_extension=nameFile.split('.')
+    # Cargar archivo
+    audio=AS.from_file(nameFile,format=name_extension[1])
+    # exportar archivo en formato especificado
+    audio.export((name_extension[0]+"1."+extension),format=extension)
     
+
+#Main
+if __name__=="__main__":
     
     #crear argumenparser para los parametros
     parser=arg.ArgumentParser(description="argumentos para la transformación")
@@ -35,7 +66,12 @@ if __name__=="__main__":
         esCarpeta=False
     except ValueError:
         esCarpeta=True
-            
+    
+    #transformar audio
+    
+    """aiff_mp3(nameFile,extension)
+    aiff_wav(nameFile,extension)"""
+    transformarAudio(nameFile,extension) 
             
     if extension is not None and  esCarpeta : 
         print("archivo: ",nameFile,"es una carpeta. pasar a ",extension)
@@ -44,25 +80,29 @@ if __name__=="__main__":
         # Mostrar los nombres de los archivos
         for archivo in archivos:
             print(archivo)
-        #funcion tranformar archivos de la carpeta en extensión
+        #funcion tranformar archivos de la carpeta en extensión   
+    #
     elif(extension is not None and not esCarpeta):
         print("no es una carpeta. pasar a ",extension)
         #funcion pasar archivo a tipo extensión
+    #
     elif(extension is None and not esCarpeta):
         print("no es carpeta, se debe pasar a mp3,ogg,wav")
         #pasar archivo a los tres tipos de archivos mp3,ogg,wav
+    #
     elif(extension is None and esCarpeta):
         print("error")
         #es una carpeta pero no se especifica la extensión, marcar error
     
-    #audio_input=AS.from_file("nombreFile")
     
     
 
 
 
     """
-    parametros: -e=wav LaProcesiondelosBorrachos.mp3
+    parametros: 
+        python dmc.py -e=wav LaProcesiondelosBorrachos.mp3
+        python dmc.py -e=mp3 "01 What Makes You Beautiful.aif"
     """
     
     
