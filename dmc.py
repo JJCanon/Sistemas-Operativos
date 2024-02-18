44cfb19c397e9f352a71aebb6d58994941e06279
Cañón <jjcanond@eafit.edu.co>
2024-02-17 23:28:26 -0500

    importacion de libreria de hilos y funciones de manejo de hilos

--- dmc.py
+++ dmc.py
@@ -2,7 +2,7 @@
 from pydub import AudioSegment as AS #Manejos de Audio
 import argparse as arg #Manejo de parametro desde terminal
 import os # Manejo de Archivos
-import aifc # Manejo de Archivos de tipo AIF 
+import threading 
 
 
 # Variables Globales
@@ -17,30 +17,46 @@ def archivosCarpeta(nameFolder):
 
     return archivos
 
-#Funcion para transformar archivo aiff a mp3(funcion temporal)
-def aiff_mp3(archivoAif,extension):
-    # Cargar archivo aiff
-    audioAiff=AS.from_file(archivoAif,format="aiff")
-    # Transformar archivo a mp3
-    audioAiff.export("cancionMp3.mp3",format=extension)
-
-#funcion para transformar archivos aiff a wav(funcion temporal)
-def aiff_wav(archivoAif,extension):
-    #cargar archivo aiff
-    audioAiff=AS.from_file(archivoAif,format="aiff")
-    #transformar el archivo a wav
-    audioAiff.export("cancionWav.wav",format=extension)
-
 # Funcion que transforma los archivos en el formato especificado
 def transformarAudio(nameFile,extension=None):
     # separar nombre y formato del archivo de entrada
     name_extension=nameFile.split('.')
     # Cargar archivo
     audio=AS.from_file(nameFile,format=name_extension[1])
-    # exportar archivo en formato especificado
-    audio.export((name_extension[0]+"1."+extension),format=extension)
+    if(extension!=None):
+        # exportar archivo en formato especificado
+        audio.export((name_extension[0]+"1."+extension),format=extension)
+    else:
+        threadsForOneFile(nameFile,name_extension[1])
     
+# Funcion para procesar multiples conversiones en hilos separados
+def thread(files,extension):
+    # Lista de Hilos
+    threads=[]
+    for file in files:
+        thread=threading.Thread(target=transformarAudio,args=(file,extension))
+        threads.append(thread)
+        thread.start()  
+    # Esperar a que todos los hilos terminen    
+    for thread in threads:
+        thread.join()
 
+# Funcion para procesar un mismo archivo en varios formatos (mp3,ogg,wav)
+def threadsForOneFile(file,extensionFile):
+    # Lista de Hilos
+    threads=[]
+    #Lista de Formatos
+    extensiones=["mp3","ogg","wav"]
+    for extension in extensiones:
+        if extension!=extensionFile:
+            threadFile=threading.Thread(target=transformarAudio,args=(file,extension))
+            threads.append(threadFile)
+            threadFile.start()
+    # Esperar a que todos los hilos terminen
+    for thread in threads:
+        thread.join()     
+        
+        
 #Main
 if __name__=="__main__":
     
@@ -66,35 +82,33 @@ if __name__=="__main__":
         esCarpeta=False
     except ValueError:
         esCarpeta=True
-    
-    
-    
-    """aiff_mp3(nameFile,extension)
-    aiff_wav(nameFile,extension)"""
      
-            
+    ##condicional
+    # Se especifica la extension y el archivo es una carpeta, 
+    # se debe pasar los archivos de la carpeta a al formato especificado       
     if extension is not None and  esCarpeta : 
-        print("archivo: ",nameFile,"es una carpeta. pasar a ",extension)
         #buscar archivos de la carpeta
         archivos=archivosCarpeta(nameFile)
         # Mostrar los nombres de los archivos
         for archivo in archivos:
             print(archivo)
         #funcion tranformar archivos de la carpeta en extensión   
-    #
+        thread(archivos,extension)
+    #se especifica la extension y no es carpeta, se debe pasar el archivo al formato especificado
     elif(extension is not None and not esCarpeta):
-        print("no es una carpeta. pasar a ",extension)
-        #funcion pasar archivo a tipo extensión
-        #transformar audio
+        #transformar audio a la extension especifica
         transformarAudio(nameFile,extension)
-    #    
+    # No se especifica la extension y no es una carpeta,
+    # se debe transformar el archivo en los formatos posibles  
     elif(extension is None and not esCarpeta):
         print("no es carpeta, se debe pasar a mp3,ogg,wav")
         #pasar archivo a los tres tipos de archivos mp3,ogg,wav
-    #
+        transformarAudio(nameFile)
+    # No se especifica la extension y el archivo es una carpeta
+    # se debe retornar error
     elif(extension is None and esCarpeta):
-        print("error")
         #es una carpeta pero no se especifica la extensión, marcar error
+        print(1)
     
     
     
